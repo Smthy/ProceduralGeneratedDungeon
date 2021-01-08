d8d90941f5a7b7e5fd972ae43411da8c979ad2e1
Smthy <61051295+Smthy@users.noreply.github.com>
2021-01-08 18:30:45 +0000

    Starting the Projecy
    Has the start of the Code and default prefabs which will change at a later date

---
+++ Assets/Scripts/DungeonGeneration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGeneration : MonoBehaviour
+{
+    public GameObject dungeonRoom, corridorNS, corridorEW;
+    private int numberOfRooms;
+
+    private void Start()
+    {
+        Instantiate(dungeonRoom);
+    }
+}
