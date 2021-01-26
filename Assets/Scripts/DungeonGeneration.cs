6e122f1f9fe9545b81ed974a1d350a1cb0dee545
Smthy <61051295+Smthy@users.noreply.github.com>
2021-01-26 14:45:07 +0000

    Attempt at moving rooms
    Changing how the room spawns, need to have it check if the new location isn't colliding with anything.

--- Assets/Scripts/DungeonGeneration.cs
+++ Assets/Scripts/DungeonGeneration.cs
@@ -5,9 +5,34 @@ using UnityEngine;
 public class DungeonGeneration : MonoBehaviour
 {
     public GameObject dungeonRoom, corridorNS, corridorEW;
-    private int numberOfRooms;
+    public int numberOfRooms;
+    public bool doorTop, doorBottom, doorLeft, doorRight;
+    public Vector3 pos;
+    public float dungeonX, dungeonZ;
 
     private void Start()
+    {
+        numberOfRooms = 7;
+        pos = dungeonRoom.GetComponent<Transform>().position;
+        dungeonX = 0f;
+        dungeonZ = 0f;
+        DungeonRoomGen();
+    }
+
+    private void DungeonRoomGen()
+    {
+        for(int roomIndex = 0; roomIndex <numberOfRooms; roomIndex++)
+        {
+            pos = new Vector3();
+            DungeonType(pos + new Vector3(dungeonX, 0, dungeonZ));
+            
+
+
+            DungeonType(pos);
+        }      
+    }
+
+    private void DungeonType(Vector3 pos)
     {
         Instantiate(dungeonRoom);
     }
