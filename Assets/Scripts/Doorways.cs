5356160092392471760f0fc3541e056210f12f4a
Smthy <61051295+Smthy@users.noreply.github.com>
2021-02-03 16:35:20 +0000

    Rooms with doors.
    Added rooms and the basics of procedural generation

---
+++ Assets/Scripts/Doorways.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorways : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Ray ray = new Ray(transform.position, transform.rotation * Vector3.forward);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(ray);
+    }
+
+}
---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dungeon : MonoBehaviour
+{
+    public Doorways[] doorways;
+    public BoxCollider boxCol;
+
+    public Bounds DungeonBounds
+    {
+        get { return boxCol.bounds; }
+    }
+}
