f466800571c7d324bae2c0ca0e465fe3a6cb8be3
Smthy <61051295+Smthy@users.noreply.github.com>
2021-02-03 12:29:08 +0000

    Corridor and Rooms
    Corridoor and rooms have walls on them now, including a light in the center, this may change later on

--- Assets/Scripts/DungeonGeneration.cs
+++ Assets/Scripts/DungeonGeneration.cs
@@ -4,36 +4,46 @@ using UnityEngine;
 
 public class DungeonGeneration : MonoBehaviour
 {
-    public GameObject dungeonRoom, corridorNS, corridorEW;
-    public int numberOfRooms;
+    public GameObject dungeonRoom;
+    public int numberOfRooms = 10;
     public bool doorTop, doorBottom, doorLeft, doorRight;
-    public Vector3 pos;
-    public float dungeonX, dungeonZ;
+    public Vector3 pos, spawn;
+    public float dungeonX, dungeonZ, xMin, xMax, zMin, zMax;
+    public Collider collisionZone;
+    private float[] position;
 
-    private void Start()
+
+    // Start is called before the first frame update
+    void Start()
     {
-        numberOfRooms = 7;
         pos = dungeonRoom.GetComponent<Transform>().position;
         dungeonX = 0f;
         dungeonZ = 0f;
         DungeonRoomGen();
     }
 
-    private void DungeonRoomGen()
+    // Update is called once per frame
+    void Update()
     {
-        for(int roomIndex = 0; roomIndex <numberOfRooms; roomIndex++)
-        {
-            pos = new Vector3();
-            DungeonType(pos + new Vector3(dungeonX, 0, dungeonZ));
-            
-
-
-            DungeonType(pos);
-        }      
+        
     }
 
-    private void DungeonType(Vector3 pos)
+    void DungeonRoomGen()
     {
-        Instantiate(dungeonRoom);
+        for(int roomIndex = 0; roomIndex < numberOfRooms; roomIndex++)
+        {
+            //pos = new Vector3();
+
+            DungeonType(pos + new Vector3(dungeonX, 0, dungeonZ));
+            dungeonX = Random.Range(xMin, xMax);
+            dungeonZ = Random.Range(zMin, zMax);
+
+            spawn = new Vector3(dungeonX, 0, dungeonZ);           
+        }
+    }  
+
+    void DungeonType(Vector3 pos)
+    {
+        Instantiate(dungeonRoom, spawn, Quaternion.Euler(0, 0, 0));
     }
 }
