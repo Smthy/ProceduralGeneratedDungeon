39824543eed562dc2ac963ad1cb80f5e6614b10b
Smthy <61051295+Smthy@users.noreply.github.com>
2021-04-04 17:27:04 +0100

    Finishing Touches

---
+++ Assets/Scripts/UnlockMouse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockMouse : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
+
